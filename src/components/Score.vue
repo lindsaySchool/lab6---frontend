6077b3844cd6cc675550b7c9b0670dba2ff17227
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-03 17:13:52 +0100

    feat: add fetch teams

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -6,6 +6,7 @@ const teams = ref([]);
 const selectedTeam = ref(null);
 const newScore = ref('');
 
+//haal alle scores op
 const fetchScore = async () => {
   try{
     const response = await fetch('http://localhost:3000/api/v1/scores');
@@ -17,8 +18,21 @@ const fetchScore = async () => {
   }
 };
 
+//haal alle teams op
+const fetchTeams = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/teams');
+    const data = await response.json();
+    teams.value = data.data.teams;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
 onMounted(() => {
   fetchScore();
+  fetchTeams();
 }); 
 
 </script>
@@ -29,12 +43,12 @@ onMounted(() => {
     <div class="updateScore">
       <!--Add een dropdown box where I can select a team-->
       <select v-model="selectedTeam">
-        <option v-for="team in teams" :key="team.id" :value="team.id">
-          {{ team.name }}
+        <option v-for="team in teams" :key="team.team" :value="team.team">
+          {{ team.team }}
         </option>
       </select>
       <input type="text" v-model="newScore" />
-      <button @click="addScore">Update</button>
+      <button @click="updateScore">Update</button>
     </div>
     <div>
       <ul>
